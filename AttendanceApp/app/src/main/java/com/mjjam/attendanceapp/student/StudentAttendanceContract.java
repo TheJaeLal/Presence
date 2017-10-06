3ed2cdaf4ba805b456da7853b98e8d69204a6704
Jerin John <jerin.j@somaiya.edu>
2017-10-06 13:14:38 +0530

    Student Attendance Monitor Added.

---
+++ AttendanceApp/app/src/main/java/com/mjjam/attendanceapp/student/StudentAttendanceContract.java
@@ -0,0 +1,18 @@
+package com.mjjam.attendanceapp.student;
+
+/**
+ * Created by Johns on 10/6/2017.
+ */
+
+public interface StudentAttendanceContract {
+
+    interface StudentAttendancePresenter {
+
+    }
+
+
+    interface StudentAttendancePresenter {
+
+    }
+}
+
