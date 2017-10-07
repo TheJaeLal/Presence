223f3a8a10c18908382fa4ded9b4109c7e7089b9
Jay Lal <jay.lal@somaiya.edu>
2017-10-07 10:29:12 +0530

    separated __update_attendance() and update_attendance(request)

--- Backend/attendance/presence/views.py
+++ Backend/attendance/presence/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from auth import views
 
 from presence import models
+import hashlib
 
 # Create your views here.
 def schedule(request):
@@ -47,4 +48,70 @@ def schedule(request):
     else:
         response["success"]=False
         response["message"]="Invalid Request"
+
+    
+
+def update_attendance(request):
+    response = {
+        'success':False
+    }
+
+    if request.method=='POST':
+        day = request.POST.get('day')
+        tid = request.POST.get('tid')
+        time = request.POST.get('time')
+        student_list = request.POST.get('students')
+        if __update_attendance(tid,student_list,time_stamp,day):
+           response['success'] = True
+
+
+    return JsonResponse(response)
+
+def __update_attendance(tid,student_list,time_stamp,day):
+    for id in student_list:
+        if verify_id(id,time_stamp):
+
+
+            #increment attendance
+            models.Attendance.objects.create(
+                student=student,
+                lecture=lecture,
+                date=date
+            )
+            #Add student to models.Attendance...
+
+
+
+def verify_id(id,time_stamp):
+    #Get the roll_no 1st 3 characters
+    roll_no = id[:3]
+    student_otp = id[3:]
+
+    #Access token is the secret key...
+    curr_student = models.Student.objects.get(roll_no = int(roll_no))
+    secret_key = curr_student.token
+
+    #Properly format time_stamp
+    time_stamp = get_unix(time_stamp)
+
+    otp_generator = str(time_stamp) + str(secret_key)
+
+    #Convert to UTF-8
+    otp_generator.encode()
+
+    if student_otp == hashlib.sha1(hash_generator).hex_digest():
+        return True
+
+    return False
+
+def get_unix(time):
+    #Convert provided time_stamp into unix time..
+    #Time is in format hh:mm 24 hour format...
+
+
+
+    return time_stamp
+
+
+
 
